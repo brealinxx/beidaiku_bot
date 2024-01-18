3c8730e3c4144471240af0fc39da2c7a86444b4d
brealinxx <brealinfee@gmail.com>
2024-01-18 17:19:16 +0800

    feat: add exiftool

--- handlers/exiftool.py
+++ handlers/exiftool.py
@@ -6,30 +6,34 @@ from pathlib import Path
 import tempfile
 
 def Exif(message: Message, bot: TeleBot) -> None:
-    """exiftool : /exif <photo>"""
-    try:
-        max_size_photo = max(message.photo, key=lambda p: p.file_size)
-        file_info = bot.get_file(max_size_photo.file_id)
-        file_path = file_info.file_path
-        downloaded_file = bot.download_file(file_path)
+     """exiftool : /exif <photo>"""
+     print("1")
+     max_size_photo = max(message.photo, key=lambda p: p.file_size)
+     print("2")
+     file_info = bot.get_file(max_size_photo.file_id)
+     print("3")
+     file_path = file_info.file_path
+     print("4")
+     downloaded_file = bot.download_file(file_path)
+     print("5")
 
-        _, file_ext = os.path.splitext(file_path)
+     _, file_ext = os.path.splitext(file_path)
+     print(file_ext)
+     with tempfile.NamedTemporaryFile(dir="/root/media", delete=False, suffix=file_ext) as temp_file:
+          temp_file.write(downloaded_file)
+          temp_file_path = temp_file.name
 
-        with tempfile.NamedTemporaryFile(delete=False, suffix=file_ext) as temp_file:
-            temp_file.write(downloaded_file)
-            temp_file_path = temp_file.name
+     photo_data_process = subprocess.Popen(['exiftool', temp_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+     stdout, stderr = photo_data_process.communicate()
+     try:
+          bot.reply_to(message, f"照片信息:\n {stdout.decode('utf-8')}")
 
-        photo_data_process = subprocess.Popen(['exiftool', temp_file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = photo_data_process.communicate()
+     except Exception as e:
+          bot.reply_to(message, f"发生错误: {str(e)}")
 
-        bot.reply_to(message, f"照片信息:\n {stdout.decode('utf-8')}")
-
-    except Exception as e:
-        bot.reply_to(message, f"发生错误: {str(e)}")
-
-    finally:
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
+     finally:
+          if os.path.exists(temp_file_path):
+               os.remove(temp_file_path)
 
 def register(bot: TeleBot) -> None:
     bot.register_message_handler(Exif, commands=["exif"], pass_bot=True)
