317439481d779775b7cb38e0cd28b36b07cbd121
brealinxx <brealinfee@gmail.com>
2024-01-19 16:58:21 +0800

    feat: add exif data cleaning function

--- handlers/exiftool.py
+++ handlers/exiftool.py
@@ -51,6 +51,7 @@ def get_stdout(bot, message, extraCmd, tempFilePath):
                 print(f"Error in file data reading process: {stderr}")
         elif extraCmd:
             for cmd in cmds:
+                print(cmd)
                 reWrite_process = subprocess.Popen(['exiftool', cmd, tempFilePath], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 stdout, stderr = reWrite_process.communicate()
                 if reWrite_process.returncode != 0:
@@ -97,7 +98,7 @@ def send_cleaned_file(bot, message, filePath):
 
 def extraCmdList(exif_data):
      pattern = re.compile(r'^-(.*?)=\s(.*?)$')
-     match = pattern.finditer('-Make= -CAM= -Model= -hh=')
+     match = pattern.finditer(exif_data)
 
      for m in match:
           print(m.group(0))
